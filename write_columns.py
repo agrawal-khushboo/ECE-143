cbec9c985f7dc2bb5e80e0510949785ae4b3834d
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-04-15 09:24:00 -0700

    Create write_columns.py

---
+++ write_columns.py
@@ -0,0 +1,24 @@
+def write_columns(data,fname):
+    """
+    Using this data
+
+data=[5,4,6,1,9,0,3,9,2,7,10,8,4,7,1,2,7,6,5,2,8,2,0,1,1,1,2,10,6,2]
+
+Write a function that can write the following formula to three columns to a comma-separated file:
+
+data_value, data_value**2, (data_value+data_value**2)/3.
+
+Here is your function signature write_columns(data,fname).
+    """
+    assert type(data)==list
+    assert type(fname)==str
+    for d in data:
+        assert type(d)==int
+    f=open('fname','w') # write mode
+    for d in data:
+        d1=d
+        d2=d**2
+        d3=(d1+d2)/3
+        f.write(str(d1)+','+str(d2)+','+str(d3)+'\n')
+    f.close()
+    
