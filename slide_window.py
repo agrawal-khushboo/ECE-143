d1135fbcf599b9be64eb978e7a1a8f5e2533811d
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-04-21 21:03:55 -0700

    Create slide_window.py

---
+++ slide_window.py
@@ -0,0 +1,14 @@
+def slide_window(x,width,increment):
+    assert increment>0
+    assert (isinstance(x, list) or isinstance(x, range))
+    if type(x)==range:
+        x=list(x)
+    assert 0<width<=len(x)
+    y=[]
+    try:
+        for i in range(0,len(x)-width+1,increment):
+            z=x[i:(i+5)]
+            y.append(z)
+        return y
+    except:
+        return []
