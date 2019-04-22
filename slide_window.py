fb37bea4a7d8ced9c4f1a922745c8f8448c8101f
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-04-21 21:39:13 -0700

    Update slide_window.py

--- slide_window.py
+++ slide_window.py
@@ -1,8 +1,16 @@
 def slide_window(x,width,increment):
+    """
+    Implement a sliding window for an arbitrary input list. 
+    The function should take the window width and the window increment as inputs and should 
+    produce a sequence of overlapping lists from the input list. For example, given x=list(range(15)), 
+    the following is the output given a window width of 5 and window increment of 2.
+    """
     assert increment>0
     assert (isinstance(x, list) or isinstance(x, range))
     if type(x)==range:
         x=list(x)
+    assert type(width)==int
+    assert type(increment)==int
     assert 0<width<=len(x)
     y=[]
     try:
