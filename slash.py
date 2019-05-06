c386c7e781294dbdbcd34e2680182d8d1e39fb82
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-05-05 23:21:45 -0700

    Update slash.py

--- slash.py
+++ slash.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 from matplotlib.pylab import subplots, cm
 def gen_rand_slash(m=6,n=6,direction='back'):
     """
@@ -7,38 +6,22 @@ def gen_rand_slash(m=6,n=6,direction='back'):
     or backslashed image (i.e., Numpy array) 
     of at least two non-zero pixels.
     """
-    arr=[]
     assert type(m)==int
     assert type(n)==int
     assert m>1
     assert n>1
     assert direction=='back' or direction=='forward'
+    row=np.random.randint(0,m-1)
+    column=np.random.randint(0,n-1)
+    slash=min((m-row),(n-column))
+    slashlen=np.random.randint(2,slash+1)
+    image=np.zeros((m,n))
+    for i in range(slashlen):
+        image[row+i][column+i]=1
     if direction=='back':
-        for i in range(m-1):
-            for j in range(n-1):
-                x=np.zeros((m,n))
-                x[i,j]=1
-                x[i+1,j+1]=1
-                arr.append(x)
-                k=min((m-i-1),(n-j-1))
-                for s in range(k-1):
-                    y=np.copy(x)
-                    y[i+2+s,j+2+s]=1
-                    arr.append(y)
-                    x=np.copy(y)
+        return image
     else:
-        for i in range(m-1,0,-1):
-            for j in range(n-1):
-                x=np.zeros((m,n))
-                x[i,j]=1
-                x[i-1,j+1]=1
-                arr.append(x)
-                k=min((i),(n-j-1))
-                for s in range(k-1):
-                    y=np.copy(x)
-                    y[i-2-s,j+2+s]=1
-                    arr.append(y)
-                    x=np.copy(y)
-    a=random.choice(arr)         
-    return a
-
+        image=np.flip(image,1)
+        return image
+        
+        
